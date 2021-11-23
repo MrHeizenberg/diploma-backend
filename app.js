2a0929e6468d643bbd8c95fb44b682be234ed6d5
MrHeizenberg <avdeevklj@mail.ru>
2021-11-24 01:25:05 +0300

    Diplomabackend+celebrate

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errorHandler } = require('./middlewares/errorHandler');
+const { limiter } = require('./middlewares/limiter');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 const app = express();
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
 });
-app.use(express.json());
 app.use(requestLogger);
+app.use(limiter);
+app.use(helmet());
+app.use(express.json());
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
-  next();
-});
+app.use(errorHandler);
 
 app.listen(PORT);
