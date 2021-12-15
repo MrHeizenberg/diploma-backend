354000b6582b2faf5844e66fd61ccda9eccfdfbf
MrHeizenberg <avdeevklj@mail.ru>
2021-12-16 02:06:25 +0300

    Cors

--- app.js
+++ app.js
@@ -9,9 +9,31 @@ const { errorHandler } = require('./middlewares/errorHandler');
 const { limiter } = require('./middlewares/limiter');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'http://localhost:3000',
+  'http://diploma-avdeev.nomoredomains.work',
+  'https://diploma-avdeev.nomoredomains.work',
+];
 const app = express();
 mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
+});
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
+  next();
 });
 app.use(requestLogger);
 app.use(limiter);
